7e12bbee2a3f863da43c8a10fe32478adc67a3be
Kaylee Ghent <kghent@neisd.net>
2024-10-02 16:15:41 -0500

    Series colors and shapes cleaned up

--- src/ScatterPlotWithLabels.java
+++ src/ScatterPlotWithLabels.java
@@ -4,17 +4,10 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.chart.labels.StandardXYItemLabelGenerator;
 import org.jfree.chart.labels.XYItemLabelGenerator;
-import org.jfree.chart.labels.ItemLabelPosition;
-import org.jfree.chart.ui.TextAnchor;
-import org.jfree.chart.ui.RectangleAnchor;
-//import org.jfree.chart.ui.ItemLabelAnchor;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -22,10 +15,15 @@ public class ScatterPlotWithLabels extends JFrame {
 
     String[] countries = {"Brazil",
                         "Turkey",
-                        "India"};
+                        "India",
+                        "Saudi Arabia",
+                        "Italy",
+                        "South Africa",
+                        "Malaysia",
+                        "South Korea"};
 
-    int[] loneliness= {50, 46, 43};
-    double[] marriageRate= {6.2, 5.9, 0};
+    int[] loneliness= {50, 46, 43, 43, 41, 40, 39, 38};
+    double[] marriageRate= {6.2, 5.9, 0, 0, 3.2, 0, 5.9, 3.7};
 
 
     public ScatterPlotWithLabels(String title) {
@@ -36,9 +34,9 @@ public class ScatterPlotWithLabels extends JFrame {
 
         // Create chart
         JFreeChart chart = ChartFactory.createScatterPlot(
-                "Scatter Plot with Labels Example", // Chart title
-                "X-Axis",                          // X-Axis Label
-                "Y-Axis",                          // Y-Axis Label
+                "Marriage Rate on % Loneliness", // Chart title
+                "% Loneliness",                          // X-Axis Label
+                "Mariage Rate per 1000 people",                          // Y-Axis Label
                 dataset,                           // Dataset
                 PlotOrientation.VERTICAL,
                 false,                             // No legend
@@ -54,6 +52,18 @@ public class ScatterPlotWithLabels extends JFrame {
         renderer.setSeriesLinesVisible(0, false); // Disable lines connecting points
        // renderer.setSeriesShapesVisible(0, true); // Enable shapes for each point
         renderer.setDefaultItemLabelsVisible(true);  // Enable item labels
+
+        for(int i = 0; i < countries.length; i ++){
+            renderer.setSeriesShape(i, new Rectangle(3, 3));
+            renderer.setSeriesPaint(i, Color.RED);
+        }
+
+     /*   renderer.setSeriesShape(0, new Rectangle(3, 3));
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesShape(1, new Rectangle(3, 3));
+        renderer.setSeriesPaint(1, Color.RED);
+        renderer.setSeriesShape(2, new Rectangle(3, 3));
+        renderer.setSeriesPaint(2, Color.RED);*/ 
 
         renderer.setDefaultItemLabelGenerator(new XYItemLabelGenerator() {
             @Override
@@ -75,8 +85,7 @@ public class ScatterPlotWithLabels extends JFrame {
 
         XYSeriesCollection dataset = new XYSeriesCollection();
         
-
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < countries.length; i++){
             XYSeries s = new XYSeries(countries[i]);
             s.add(loneliness[i], marriageRate[i]);
             dataset.addSeries(s);

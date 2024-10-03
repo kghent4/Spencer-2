fc8239a918791d2fcea7514faa50301577488eb8
spencerbalmer <cougrexs@gmail.com>
2024-10-03 12:58:55 -0500

    NEW DATA

--- src/ScatterPlotWithLabels.java
+++ src/ScatterPlotWithLabels.java
@@ -15,17 +15,10 @@ import java.awt.geom.Ellipse2D;
 
 public class ScatterPlotWithLabels extends JFrame {
 
-    String[] dataLabels = {"Brazil",
-                        "Turkey",
-                        "India",
-                        "Saudi Arabia",
-                        "Italy",
-                        "South Africa",
-                        "Malaysia",
-                        "South Korea"};
+    String[] dataLabels = {"Brazil","Turkey","India","Saudi Arabia","Italy","South Africa","Malaysia", "South Korea","Chile","Peru","France","Argentina","Mexico","Great Britain","Canada","United States","Singapore","Australia","Hungary","Belgium","Spain","Germany","China","Sweden","Russia","Poland","Japan","Netherlands"};
 
-    int[] xVals= {50, 46, 43, 43, 41, 40, 39, 38};
-    double[] yVals= {6.2, 5.9, 0, 0, 3.2, 0, 5.9, 3.7};
+    int[] xVals= {50, 46, 43, 43, 41, 40, 39, 38, 38, 37, 36, 35, 34, 34, 31, 31, 30, 30, 29, 28, 27, 26, 26, 25, 25, 23, 16, 15};
+    double[] yVals= {6.2, 5.9, 0, 0, 3.2, 0, 5.9, 3.7, 3.3, 2.5, 3.1, 2.9, 3.3, 4.4, 0, 5.1, 6.8, 4.2, 6.9, 3.4, 3.1, 4.2, 0, 3.8, 0.0, 3.9, 4.1, 3.5};
 
 
     public ScatterPlotWithLabels(String title) {
